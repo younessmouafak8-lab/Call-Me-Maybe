b458075ca6524ef3dc161c502c0bd0489bf1238d
ymouafak <ymouafak@e2r9p4.1337.ma>
2026-07-04 23:21:50 +0100

    call me rn

--- src/__main__.py
+++ src/__main__.py
@@ -27,13 +27,28 @@ def prompt_builder(prompt, functions):
     """
 
 
+def get_name(token: str, string: str):
+    name = ""
+    if '",' not in token:
+        return name
+    else:
+        name = string.split('"')[7]
+    return name
+
+
+def complete_parameters(parameters: dict, name):
+    params = parameters[name]
+    for param in params.keys():
+        yield f'"{param}": '
+
+
 def main():
     p = parsing()
     if not p:
         return
     prompts, functions, output_file = p
     func_def = [f"{func['name']}: {func['parameters']}" for func in functions]
-    # prompt_builder(func_def)
+    params = {func["name"]: func["parameters"] for func in functions}
     m = model()
     # vocabulary_path = m.get_path_to_vocab_file()
     # with open(vocabulary_path, "r") as f:
@@ -41,28 +56,46 @@ def main():
     # vocabulary = {value: key for key, value in vocabulary.items()}
     output = []
     # start  =time
-    for prompt in prompts[8:]:
-        string = f'{{"prompt": "{prompt}", "name":'
+    for prompt in prompts:
+        string = f'{{"prompt": "{prompt}", "name": "'
         # print(string,  end="", flush=True)
-        tensor = m.encode(prompt_builder(prompt, func_def) + (string))
-        # tensor = m.encode(prompt_builder(prompt, func_def))
-        ids = tensor.tolist()[0]
-        generated_ids = []
+        ids = m.encode(prompt_builder(prompt, func_def) + (string)).tolist()[0]
+        # generated_ids = []
+        name_generated = False
+        param_generated = False
         while 1:
             logits = m.get_logits_from_input_ids(ids)
             next_token_id = int(np.argmax(logits))
             ids.append(next_token_id)
-            generated_ids.append(next_token_id)
-            string += m.decode(next_token_id)
+            # generated_ids.append(next_token_id)
+            value = m.decode(next_token_id)
+            string += value
+            if not name_generated:
+                name = get_name(value, string)
+            if name:
+                name_generated = True
+            if name_generated and "parameters" not in string:
+                parameters = ' "parameters": {'
+                ids += m.encode(parameters).tolist()[0]
+                string += parameters
+            if name_generated and "parameters" in string:
+                if not param_generated:
+                    prm = complete_parameters(params, name)
+                    param_generated = True
+                try
+                temp = next(prm)
+                ids += m.encode(temp).tolist()[0]
+                string += temp
             print(string)
             try:
                 if string.endswith("\n"):
                     break
-                obj = json.loads(string)
+                json.loads(string)
                 break
             except json.JSONDecodeError:
                 pass
-        output.append(obj)
+        output.append(json.loads(string))
+        print(name)
         print("###############################################")
     with open("data/output.json", "w") as f:
         json.dump(output, f)
